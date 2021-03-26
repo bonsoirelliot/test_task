b3dce532c444c05b33a7021b605d1ee655a35a3d
bonsoirelliot <privetdek16@gmail.com>
2021-03-26 14:54:20 +0500

    Commit

--- lib/pages/shop_screen.dart
+++ lib/pages/shop_screen.dart
@@ -4,6 +4,7 @@ import 'package:scoped_model/scoped_model.dart';
 import 'package:test_task/models/shop_screen_model.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class ShopScreen extends StatelessWidget {
   ShopScreenModel model = new ShopScreenModel();
@@ -31,14 +32,13 @@ class ShopScreen extends StatelessWidget {
                       : AlwaysScrollableScrollPhysics(),
                   itemCount: model.pages_count,
                   itemBuilder: (context, position) {
-                    return GridView.count(
+                    return StaggeredGridView.countBuilder(
                       controller: _controller,
-                      childAspectRatio:
-                          (MediaQuery.of(context).size.height / 3) /
-                              (MediaQuery.of(context).size.width * 1.15),
-                      mainAxisSpacing: 0,
                       crossAxisCount: 2,
-                      children: List.generate(model.item_count(position), (i) {
+                      itemCount: model.item_count(position),
+                      staggeredTileBuilder: (index) => new StaggeredTile.fit(1),
+                      mainAxisSpacing: 20,
+                      itemBuilder: (context, i) {
                         _controller.addListener(() {
                           if (_controller.position.pixels ==
                               _controller.position.maxScrollExtent) {
@@ -51,55 +51,57 @@ class ShopScreen extends StatelessWidget {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Center(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.circular(20.0),
-                                    child: CachedNetworkImage(
-                                      width: MediaQuery.of(context).size.width *
-                                          0.45,
-                                      height:
-                                          MediaQuery.of(context).size.height /
-                                              3.5,
-                                      fit: BoxFit.cover,
-                                      imageUrl:
-                                          snapshot.data[i + 6 * position].image,
-                                      placeholder: (context, url) =>
-                                          CircularProgressIndicator(),
-                                      errorWidget: (context, url, error) =>
-                                          Icon(Icons.error),
-                                    ),
-                                  ),
-                                  Text(
-                                    snapshot.data[i + 6 * position].article
-                                        .toString(),
-                                    style: TextStyle(
-                                        color: Colors.black26,
-                                        fontFamily: 'ProximaNova'),
-                                  ),
-                                  Text(
-                                    snapshot.data[i + 6 * position].name,
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontFamily: 'ProximaNova'),
-                                    overflow: TextOverflow.visible,
-                                  ),
-                                  Text(
-                                    NumberFormat.currency(locale: 'ru')
-                                        .format(snapshot.data[i].price),
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontFamily: 'ProximaNova',
-                                        fontWeight: FontWeight.bold),
-                                    textAlign: TextAlign.left,
-                                  ),
-                                ],
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20.0),
+                                child: CachedNetworkImage(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.45,
+                                  height:
+                                      MediaQuery.of(context).size.height / 3,
+                                  fit: BoxFit.cover,
+                                  imageUrl:
+                                      snapshot.data[i + 6 * position].image,
+                                  placeholder: (context, url) =>
+                                      CircularProgressIndicator(),
+                                  errorWidget: (context, url, error) =>
+                                      Icon(Icons.error),
+                                ),
+                              ),
+                            ),
+                            Center(
+                              child: Text(
+                                snapshot.data[i + 6 * position].article
+                                    .toString(),
+                                style: TextStyle(
+                                    color: Colors.black26,
+                                    fontFamily: 'ProximaNova'),
+                                textAlign: TextAlign.left,
+                              ),
+                            ),
+                            Center(
+                              child: Text(
+                                snapshot.data[i + 6 * position].name,
+                                //softWrap: false,
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontFamily: 'ProximaNova'),
+                                //textAlign: TextAlign.left,
+                                //overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            Center(
+                              child: Text(
+                                NumberFormat.currency(locale: 'ru')
+                                    .format(snapshot.data[i].price),
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontFamily: 'ProximaNova',
+                                    fontWeight: FontWeight.bold),
                               ),
                             ),
                           ],
                         );
-                      }),
+                      },
                     );
                   },
                 );
